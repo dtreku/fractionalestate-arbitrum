2727235e04dc502072cc4a973dfbf87377558ec7
Daniel Treku <37296448+dtreku@users.noreply.github.com>
2026-06-22 00:58:22 -0400

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@react-native-async-storage/async-storage': false,
+    };
     return config;
   },
 };
